3af5e7ee175ce517b0cffdc18f60c9ef4b1bcd24
Devin Graham <thedevingraham@gmail.com>
2022-02-27 22:51:50 -0500

    add user interface for win state and counting

--- Rollaball/Assets/Scripts/PlayerController.cs
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
 
@@ -16,6 +20,12 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        count = 0;
+
+        SetCountText();
+
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -24,6 +34,16 @@ public class PlayerController : MonoBehaviour
 
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if(count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 
     void FixedUpdate()
@@ -38,6 +58,9 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count++;
+
+            SetCountText();
         }
         
     }
